3859b35a24a3d607c9e93ae13a9163998e755b7d
Brett Davis <brett.davis5511@gmail.com>
2021-11-04 17:27:24 -0500

    Book DAO

---
+++ src/main/java/com/revature/dao/BookDAO.java
@@ -0,0 +1,84 @@
+package com.revature.dao;
+
+import com.revature.model.Book;
+import com.revature.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDAO implements DAO<Book>{
+    @Override
+    public void insert(Book obj) {
+
+    }
+
+    @Override
+    public Book readById(Book obj) {
+        String sql = "select * from books";
+        Connection connection = ConnectionFactory.getConnection();
+        Book book = new Book();
+
+        try{
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                book.setIsbnNumber(rs.getInt(1));
+                book.setTitle(rs.getString(2));
+                book.setAuthor(rs.getString(3));
+                book.setGenre(rs.getString(4));
+                book.setPrice(rs.getDouble(5));
+                book.setDesc(rs.getString(6));
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return book;
+    }
+
+    public List<Book> readByGenre(){
+        String sql = "select * from books";
+        Connection connection = ConnectionFactory.getConnection();
+        List<Book> books = new ArrayList<>();
+
+        try{
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Book book = new Book();
+                book.setIsbnNumber(rs.getInt(1));
+                book.setTitle(rs.getString(2));
+                book.setAuthor(rs.getString(3));
+                book.setGenre(rs.getString(4));
+                book.setPrice(rs.getDouble(5));
+                book.setDesc(rs.getString(6));
+                books.add(book);
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return books;
+    }
+
+    @Override
+    public List<Book> readAll() {
+        return null;
+    }
+
+    @Override
+    public Book update(Book obj) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Book obj) {
+        return false;
+    }
+}
